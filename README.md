535b270fd355b5f8c7d35869abce3efb36d989ed
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2017-04-26 10:22:41 -0700

    First version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # eslint-config-fandom
 ESLint ruleset for Fandom based on https://github.com/Wikia/javascript-style-guide
+
+## Usage
+
+### package.json
+```json
+{
+  "devDependencies": {
+    "eslint-config-fandom": "git://github.com/Wikia/eslint-config-fandom.git#1.0.0",
+    "eslint": "^3.19.0"
+  }
+}
+```
+
+### .eslintrc
+```json
+{
+  "extends": "fandom",
+}
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  'root': true,
+  'parser': 'babel-eslint',
+  'extends': 'airbnb',
+  'ext': [
+    'js', 'jsx', 'es6'
+  ],
+  'rules': {
+    'indent': [2, 'tab', {'VariableDeclarator': 1, 'SwitchCase': 1}],
+    'max-len': [2, 120, 2],
+    'object-curly-spacing': [2, 'never'],
+    'quotes': [2, 'single', {'allowTemplateLiterals': true}],
+    'no-tabs': 0,
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'never',
+      'exports': 'always-multiline',
+      'functions': 'ignore',
+    }],
+    'require-jsdoc': ['error', {
+      'require': {
+          'FunctionDeclaration': true,
+          'MethodDefinition': true,
+          'ClassDeclaration': true,
+          'ArrowFunctionExpression': true
+      }
+    }],
+    'valid-jsdoc': ['error', {
+      'matchDescription': '',
+      'requireReturn': true,
+      'requireReturnType': true,
+      'requireParamDescription': false,
+      'requireReturnDescription': false,
+      'prefer': {
+        'arg': 'param',
+        'argument': 'param',
+        'function': 'function',
+        'class': 'constructor',
+        'return': 'returns',
+        'virtual': 'abstract'
+      },
+      'preferType': {
+        'Boolean': 'boolean',
+        'Number': 'number',
+        'object': 'Object',
+        'String': 'string'
+      }
+    }]
+  }
+};
